a6a9b2d2e7b82d9bf2391210d5e2c5ecd2385dcd
Pascal Ballet <pascal.ballet@gmail.com>
2024-03-14 13:25:38 +0100

    First push

---
+++ dive_pp.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in 10:
+		step()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# ***********************
+# Model parameters
+# ***********************
+var time:float = 0.0
+var dt:float = 0.001
+# Air compartment parameters
+var pp_O2_air_t0:float = 1.0
+var pp_O2_air_t1:float = 0.0
+
+# Airways compartment parameters
+var pp_O2_aw_t0:float = 1.0
+var pp_O2_aw_t1:float = 0.0
+
+# Alveols compartment parameters
+var pp_O2_alv_t0:float = 1.0
+var pp_O2_alv_t1:float = 0.0
+
+# ***********************
+# Compartment functions
+# ***********************
+func airways():
+	var delta = (0.8 * pp_O2_aw_t0 + 0.1 * pp_O2_air_t0 + 0.1 * pp_O2_alv_t0)*dt
+	pp_O2_aw_t1 = pp_O2_aw_t0 + delta
+
+# Execute One step (dt) of the model
+func step():
+	# Compute one step
+	airways()
+	
+	# Next step
+	pp_O2_air_t0 	= pp_O2_air_t1
+	pp_O2_aw_t0 	= pp_O2_aw_t1
+	pp_O2_alv_t0 	= pp_O2_alv_t1
+	
+	time = time + dt
